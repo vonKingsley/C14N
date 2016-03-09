1e08b29c0583be2a4456d37e879eb4e3247cb1a3
kinglewi <kinglewi@cisco.com>
2016-03-08 19:36:09 -0800

    update syntax for 13

--- spec/c14n_spec.cr
+++ spec/c14n_spec.cr
@@ -1,9 +1,10 @@
 require "./spec_helper"
-#tests are loosely based off of W3C site 
-#http://www.w3.org/2008/xmlsec/Drafts/c14n-20/test-cases/#sec-Example-UTF8
 
-  def setup
-    test_xml = <<-XML
+# tests are loosely based off of W3C site
+# http://www.w3.org/2008/xmlsec/Drafts/c14n-20/test-cases/#sec-Example-UTF8
+
+def setup
+  test_xml = <<-XML
 <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE doc SYSTEM "doc.dtd">
       <doc>
@@ -22,46 +23,44 @@ require "./spec_helper"
       <!-- Comment 2 -->
 
       <!-- Comment 3 -->
-      XML
-    parsed = XML.parse(test_xml)
+XML
+  parsed = XML.parse(test_xml)
+end
+
+describe XML::C14N do
+  describe "#canonicalize" do
+    parsed = setup
+    canonicalized = parsed.canonicalize
+
+    it "should not contain a xml declaration" do
+      canonicalized.should_not match(/^<\?xml/m)
+    end
+
+    it "should not contain a DOCTYPE" do
+      canonicalized.should_not match(/^DOCTYPE/m)
+    end
+
+    it "should not contain comments" do
+      canonicalized.should_not match(/Comment/m)
+    end
+
+    it "should not contain CDATA tags" do
+      canonicalized.should_not match(/^<!\[CDATA\[/m)
+    end
+
+    it "should have utf8 codepoint" do
+      canonicalized.to_s.should contain "☠"
+    end
   end
 
-  describe XML::C14N do
-    describe "#canonicalize" do
+  describe "#canonicalize!" do
+    it "changes the object" do
       parsed = setup
-      canonicalized = parsed.canonicalize
-
-      it "should not contain a xml declaration" do
-        canonicalized.should_not match( /^<\?xml/m )
-      end
-
-      it "should not contain a DOCTYPE" do
-        canonicalized.should_not match( /^DOCTYPE/m )
-      end
-
-      it "should not contain comments" do 
-        canonicalized.should_not match( /Comment/m )
-      end
-
-      it "should not contain CDATA tags" do
-        canonicalized.should_not match( /^<!\[CDATA\[/m )
-      end
-
-      it "should have utf8 codepoint" do
-        canonicalized.to_s.should contain "☠"
-      end
-
+      puts parsed
+      parsed.canonicalize!
+      puts "\n\n"
+      puts parsed
+      parsed.should be_a(XML::Node)
     end
-
-    describe "#canonicalize!" do
-      it "changes the object" do
-        parsed = setup
-        puts parsed
-        parsed.canonicalize!
-        puts "\n\n"
-        puts parsed
-        parsed.should be_a(XML::Node)
-      end
-    end
-
   end
+end
--- src/c14n/lib_c14n.cr
+++ src/c14n/lib_c14n.cr
@@ -3,14 +3,13 @@ require "./mode"
 
 @[Link("xml2")]
 lib LibC14N
-
   alias OutputWriteCallback = (Void*, UInt8*, Int32) -> Int32
   alias OutputCloseCallback = (Void*) -> Int32
-  #alias IsVisibleCallback   = (Void*, LibXML::Node*, LibXML::Node*) -> Int32
+  # alias IsVisibleCallback   = (Void*, LibXML::Node*, LibXML::Node*) -> Int32
 
   type CharEncodingHandler = Void*
   type OutputBuffer = Void*
-  #struct OutputBuffer
+  # struct OutputBuffer
   #  context : Void*
   #  writecallback : OutputWriteCallback
   #  closecallback : OutputCloseCallback
@@ -19,14 +18,14 @@ lib LibC14N
   #  conv    : UInt8*
   #  written : Int32
   #  error   : Int32
-  #end
+  # end
 
-  #fun xmlAllocOutputBuffer(encoder : Void*) : OutputBuffer*
+  # fun xmlAllocOutputBuffer(encoder : Void*) : OutputBuffer*
   fun xmlOutputBufferClose(out : OutputBuffer*) : Int32
   fun xmlOutputBufferCreateIO(iowrite : OutputWriteCallback, ioclose : OutputCloseCallback, ioctx : Void*, encoder : Void*) : OutputBuffer*
-  #fun xmlOutputBufferCreateBuffer(buffer : XMLBuffer*, encoder : Void*) : OutputBuffer*
-  #fun xmlC14NDocDumpMemory(doc : LibXML::Node*, nodes : LibXML::NodeSet*, mode : XML::C14N::Mode, inclusive_ns_prefixes : UInt8**, with_comments : Int32, doc_txt_prt : UInt8**) : Int32
-  #fun xmlC14NDocSave(doc : LibXML::Node*, nodes : LibXML::NodeSet*, mode : XML::C14N::Mode , inclusive_ns_prefixes : UInt8**, with_comments : Int32, filename : UInt8*, compression : Int32 ) : Int32
+  # fun xmlOutputBufferCreateBuffer(buffer : XMLBuffer*, encoder : Void*) : OutputBuffer*
+  # fun xmlC14NDocDumpMemory(doc : LibXML::Node*, nodes : LibXML::NodeSet*, mode : XML::C14N::Mode, inclusive_ns_prefixes : UInt8**, with_comments : Int32, doc_txt_prt : UInt8**) : Int32
+  # fun xmlC14NDocSave(doc : LibXML::Node*, nodes : LibXML::NodeSet*, mode : XML::C14N::Mode , inclusive_ns_prefixes : UInt8**, with_comments : Int32, filename : UInt8*, compression : Int32 ) : Int32
   fun xmlC14NDocSaveTo(doc : LibXML::Node*, nodes : LibXML::NodeSet*, mode : XML::C14N::Mode, inclusive_ns_prefixes : UInt8**, with_comments : Int32, buf : OutputBuffer*) : Int32
-  #fun xmlC14NExecute(doc : LibXML::Node*, is_visible_callback : IsVisibleCallback, user_data : Void*, mode : XML::C14N::Mode, inclusive_ns_prefixes : UInt8**, with_comments : Int32, buf : OutputBuffer*) : Int32
+  # fun xmlC14NExecute(doc : LibXML::Node*, is_visible_callback : IsVisibleCallback, user_data : Void*, mode : XML::C14N::Mode, inclusive_ns_prefixes : UInt8**, with_comments : Int32, buf : OutputBuffer*) : Int32
 end
